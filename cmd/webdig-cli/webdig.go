8c41be6311597857d1e7f4d8ca17d311e80fba5d
Erik Svensson <arizon.es@gmail.com>
2026-03-27 22:29:49 +0100

    Add help for cli tool

--- cmd/webdig-cli/webdig.go
+++ cmd/webdig-cli/webdig.go
@@ -10,20 +10,21 @@ import (
 	"github.com/arizon-dread/webdig-backend/pkg/types"
 )
 
+var (
+	s = flag.String("s", "", "URL to webdig SERVER for this call, including contextpath. Ex: -s http://localhost:8080/api/dig")
+	c = flag.Bool("c", false, "Configure the server setting in your local config for subsequent requests, -s http://webdig.server.name/api/dig is required to use -c.\nThe server.yaml config file is stored in:\nLinux: /home/<username>/.config/webdig/\nMacOS: /Users/<username>/Library/Application Support/webdig/\nWindows: C:\\Users\\<username>\\AppData\\webdig\\\n")
+	n = flag.Bool("n", false, "Lookup cName. Using this flag makes the command slower but also looks up the CNAME (if any) and which A-record it points to\n")
+)
+
 func main() {
-	var s string
-	var c bool
-	var n bool
-	flag.StringVar(&s, "s", "", "server url to webdig server for this call")
-	flag.BoolVar(&c, "c", false, "configure the --server setting in your local config for subsequent usage")
-	flag.BoolVar(&n, "n", false, "lookup CNAME. using this flag makes the command slower but also looks up the CNAME and which A-record it points to.")
+	flag.Usage = setHelpText
 	flag.Parse()
 	// get all remaining arguments
 	args := flag.Args()
 	var addr string
 	// make sure that a lookup parameter is supplied
 	if len(args) > 1 {
-		fmt.Printf("You must supply the lookup address as the last parameter\n")
+		fmt.Println("You must supply the lookup address as the last parameter")
 		os.Exit(1)
 	}
 	for _, a := range args {
@@ -32,60 +33,90 @@ func main() {
 			break
 		}
 	}
+
 	if len(addr) == 0 {
-		fmt.Println("supply a single IP or DNS FQDN to perform a lookup")
+		fmt.Println("Supply a single IP or DNS FQDN to perform a lookup")
 		os.Exit(1)
 	}
 	conf := &types.ServerConf{}
+
 	// if a server address is supplied, make the request to that server.
-	if s != "" {
+	if *s != "" {
 		// call webdig with specified address
-		conf.Server = s
-		if c {
+		conf.Server = *s
+		if *c {
 			// try to create the config file
-			err := handlers.SaveConf(s)
+			err := handlers.SaveConf(*s)
 			if err != nil {
-				fmt.Printf("%v\n", err)
+				fmt.Printf("Error trying to save your config, %v\n", err)
 				os.Exit(2)
 			}
 		}
 	} else {
 		savedConf, err := handlers.EnsureConfig(nil)
 		if err != nil {
-			fmt.Printf("couldn't find configured service, use -c -s full-url-to-server to configure, %v\n", err)
+			fmt.Printf("Couldn't find configured service, use -c -s full-url-to-server to configure, see -h for help %v\n", err)
 			os.Exit(3)
 		}
 		conf.Server = savedConf.Server
 	}
 	req := types.Req{
 		Host:  addr,
-		CNAME: &n,
+		CNAME: n,
 	}
 	resp, err := handlers.MakeCall(req, conf)
 	if err != nil {
-		fmt.Printf("%v\n", err)
+		// Error formatting is done inside handlers.MakeCall(), just output whatever text we get in the err.
+		fmt.Printf("%v", err)
 		os.Exit(4)
 	}
-	var respText string
+	var respText strings.Builder
 
 	for _, r := range resp.Results {
 		if len(r.IPAddresses) == 0 && len(r.DnsNames) == 0 {
 			continue
 		}
-		respText += fmt.Sprintf("Name: %v\n", r.Name)
+		fmt.Fprintf(&respText, "Name: %v\n", r.Name)
 		if len(r.DnsNames) > 0 {
 			dnsNames := strings.Join(r.DnsNames, ", ")
-			respText += fmt.Sprintf("DNS: %v", dnsNames)
+			fmt.Fprintf(&respText, "DNS: %v", dnsNames)
 		}
 		if len(r.IPAddresses) > 0 {
 			ipAddrs := strings.Join(r.IPAddresses, ", ")
-			respText += fmt.Sprintf("IP: %v", ipAddrs)
+			fmt.Fprintf(&respText, "IP: %v", ipAddrs)
 		}
 		if len(r.Cname) > 0 {
-			respText += fmt.Sprintf("\nCNAME for %v", r.Cname)
+			fmt.Fprintf(&respText, "\nCNAME for %v", r.Cname)
 		}
-		respText += "\n-----\n"
+		respText.WriteString("\n-----\n")
 	}
-	fmt.Printf("\n%v", respText)
+	fmt.Printf("\n%v", respText.String())
+	os.Exit(0)
+}
+
+func setHelpText() {
+	fmt.Fprintf(flag.CommandLine.Output(), `wdig - a commandline client for the WebDig API
+
+Usage: %s [options] <arg>
+
+options:
+
+`, os.Args[0])
+	flag.PrintDefaults()
+	fmt.Fprintf(flag.CommandLine.Output(), `
+Lookup: 
+	<arg> is the ip or dnsName you want to perform lookup on. 
+
+You must point your wdig client to a WebDig API, example:
+
+	wdig -c -s http://localhost:8080/api/dig
+
+You can also skip -c to just run wdig towards a different API address on this call, without persisting the address to your config.
+After configuring your client, perform lookups like this:
+
+	wdig www.google.com
+	wdig 192.168.1.1
+	wdig -n www.google.com
+	wdig -s http://10.10.33.33/api/dig www.google.com`)
 	os.Exit(0)
 }
